0d04b0cb4ed41632c7ab46d1d4f73265b2de7298
Caleb Long <caleblong199@gmail.com>
2024-09-18 13:00:32 +0100

    Computer World - Updated User Interface
    Changed the UI to better display what the final design will mostly represent. All changes can be viewed via the website in the readme.

--- src/views/OrdersListView.vue
+++ src/views/OrdersListView.vue
@@ -17,16 +17,47 @@ onMounted(() => {
 </script>
 
 <template>
-  <h1>COMPUTER WORLD - ORDERS</h1>
+  <h1>COMPUTER WORLD</h1>
+  <h2>ORDERS</h2>
   <div class="orders">
-    <OrderDisplay v-for="order in orders" :key="order.orderID" :order="order" />
+    <div class="card-overflow">
+        <OrderDisplay v-for="order in orders" :key="order.orderID" :order="order" />
+    </div>
+  </div>
+  <div class="form-layout">
+    <form class="card">
+      <h4>ORDER MANAGER</h4>
+<!--      <input type="checkbox">-->
+<!--      <button type="submit" class="btn btn-primary">Update</button>-->
+<!--      <button type="submit" class="btn btn-danger">Delete</button>-->
+    </form>
   </div>
 </template>
 
 <style scoped>
 .orders {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  float: left;
+  position: relative;
+  left: 34%;
+}
+.form-layout {
+  float: left;
+  position: relative;
+  left: 34%;
+}
+.card {
+  width: 350px;
+  height: 440px;
+  margin-bottom: 25px;
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,0.2);
+  padding: 2px 16px;
+  border-radius: 5px; /* 5px rounded corners */
+}
+.card-overflow {
+  width: 250px;
+  height: 450px;
+  overflow:scroll;
+  padding: 5px;
+
 }
 </style>
--- src/views/ProductsListView.vue
+++ src/views/ProductsListView.vue
@@ -17,16 +17,47 @@ onMounted(() => {
 </script>
 
 <template>
-  <h1>COMPUTER WORLD - PRODUCTS</h1>
+  <h1>COMPUTER WORLD</h1>
+  <h2>PRODUCTS</h2>
   <div class="products">
-    <ProductsDisplay v-for="product in products" :key="product.id" :product="product" />
+    <div class="card-overflow">
+      <ProductsDisplay v-for="product in products" :key="product.id" :product="product" />
+    </div>
+  </div>
+  <div class="form-layout">
+    <form class="card">
+      <h4>PRODUCT MANAGER</h4>
+      <!--      <input type="checkbox">-->
+      <!--      <button type="submit" class="btn btn-primary">Update</button>-->
+      <!--      <button type="submit" class="btn btn-danger">Delete</button>-->
+    </form>
   </div>
 </template>
 
 <style scoped>
 .products {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  float: left;
+  position: relative;
+  left: 34%;
+}
+.form-layout {
+  float: left;
+  position: relative;
+  left: 34%;
+}
+.card {
+  width: 350px;
+  height: 440px;
+  margin-bottom: 25px;
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,0.2);
+  padding: 2px 16px;
+  border-radius: 5px; /* 5px rounded corners */
+}
+.card-overflow {
+  width: 250px;
+  height: 450px;
+  overflow:scroll;
+  padding: 5px;
+
 }
 </style>
